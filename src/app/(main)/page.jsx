b8cde6b24c29f2e2e44bb3fbf2ce24ddc990e6b7
Batuhan Y. Yılmaz <batuhan@yyilmaz.com.tr>
2025-08-08 21:42:21 +0300

    Revert "Start base project"
    This reverts commit 0731bfb1134f24ebd595bf61b3335e06823d930a.

--- src/app/(main)/page.jsx
+++
@@ -1,9 +0,0 @@
-import HomeContainer from "@/components/containers/home-container"
-
-export default function Home() {
-    return (
-        <>
-            <HomeContainer />
-        </>
-    );
-}
